b274c9c08fcd4a57c0b8f1b869b866f39ff5b3ab
Simon <simon.nordstrom@chasacademy.se>
2020-10-01 20:52:21 +0200

    fix merge conflict in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,3 @@
-<<<<<<< Updated upstream
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
-import Header from "./components/shared/Header";
-import Register from "./components/register/Register";
-import Login from "./components/login/Login";
-=======
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
@@ -17,7 +9,6 @@ const Container = styled.div`
   display: flex;
   flex-direction: row;
 `;
->>>>>>> Stashed changes
 
 const App = () => {
   return (
@@ -25,16 +16,16 @@ const App = () => {
       <Router>
         <Header />
         <Switch>
-          <Route path='/about'>
+          <Route path="/about">
             <About />
           </Route>
-          <Route path='/login'>
+          <Route path="/login">
             <Login />
           </Route>
-          <Route path='/register'>
+          <Route path="/register">
             <Register />
           </Route>
-          <Route exact path='/'>
+          <Route exact path="/">
             <Home />
           </Route>
         </Switch>
